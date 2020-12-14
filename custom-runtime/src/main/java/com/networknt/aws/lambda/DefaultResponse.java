153b19da0b643592c038b2e6325742f318051ccb
Steve Hu <stevehu@gmail.com>
2020-12-13 21:58:35 -0500

    fixes #3 add custom-runtime module

---
+++ custom-runtime/src/main/java/com/networknt/aws/lambda/DefaultResponse.java
@@ -0,0 +1,50 @@
+package com.networknt.aws.lambda;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+
+@JsonDeserialize(builder = DefaultResponse.Builder.class)
+public class DefaultResponse {
+
+    private int code;
+    private String message;
+
+    public DefaultResponse(DefaultResponse.Builder builder) {
+        this.code = builder.code;
+        this.message = builder.message;
+    }
+
+    public static DefaultResponse.Builder builder() {
+        return new DefaultResponse.Builder();
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @JsonPOJOBuilder(withPrefix = "")
+    public static class Builder {
+        private int code;
+        private String message;
+
+        public Builder() { }
+
+        public DefaultResponse.Builder code(int code) {
+            this.code = code;
+            return this;
+        }
+
+        public DefaultResponse.Builder message(String message) {
+            this.message = message;
+            return this;
+        }
+
+        public DefaultResponse build() {
+            return new DefaultResponse(this);
+        }
+    }
+}

cd3a87e9ef86343e509ad27aae32d64694a071da
Steve Hu <stevehu@gmail.com>
2020-12-15 11:35:11 -0500

    fixes #8 Update the jwt-authorizer to verify JWT token with jwk support

--- jwt-authorizer/Authorizer/src/main/java/com/networknt/aws/lambda/Authorizer.java
+++ jwt-authorizer/Authorizer/src/main/java/com/networknt/aws/lambda/Authorizer.java
@@ -1,57 +1,104 @@
 package com.networknt.aws.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.networknt.exception.ExpiredTokenException;
+import com.networknt.status.Status;
+import com.networknt.utility.Constants;
+import org.jose4j.jwt.JwtClaims;
+import org.jose4j.jwt.MalformedClaimException;
+import org.jose4j.jwt.consumer.InvalidJwtException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import static com.networknt.aws.lambda.AuthPolicy.PolicyDocument.getAllowOnePolicy;
 import static com.networknt.aws.lambda.AuthPolicy.PolicyDocument.getDenyOnePolicy;
 
 public class Authorizer implements RequestHandler<APIGatewayProxyRequestEvent, AuthPolicy> {
+    private static Logger logger = LoggerFactory.getLogger(Authorizer.class);
+    private static String STATUS_INVALID_AUTH_TOKEN = "ERR10000";
+    private static String STATUS_AUTH_TOKEN_EXPIRED = "ERR10001";
+
+    public static JwtVerifier jwtVerifier;
+    static {
+        jwtVerifier = new JwtVerifier();
+    }
+
     ObjectMapper objectMapper = new ObjectMapper();
 
     public AuthPolicy handleRequest(APIGatewayProxyRequestEvent request, Context context) {
-        LambdaLogger logger = context.getLogger();
         try {
-            logger.log("EVENT: " + objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(request));
+            logger.debug(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(request));
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
-        Map<String, String> headers = request.getHeaders();
-        String authorization = headers.get("Authorization");
-        String scopeToken = headers.get("X-Scope-Token");
-
-        String primaryToken = authorization.substring(7);
-        String secondaryToken = scopeToken.substring(7);
-
-        Map<String, String> ctx = new HashMap<>();
-        ctx.put("cid", "client_id");
-        ctx.put("uid", "user_id");
-        ctx.put("primaryScopes", "portal.w portal.r");
-        ctx.put("secondaryScopes", "pet.w pet.r");
-
-        String principalId = "user_id"; // principalId should be user_id (AC) or client_id (CC) in the JWT token
-
         APIGatewayProxyRequestEvent.ProxyRequestContext proxyContext = request.getRequestContext();
-
         String region = System.getenv("AWS_REGION");  // use the env to get the region for REQUEST authorizer.
         String accountId = proxyContext.getAccountId();
         String apiId = proxyContext.getApiId();
         String stage = proxyContext.getStage();
         String httpMethod = proxyContext.getHttpMethod();
         String arn = String.format("arn:aws:execute-api:%s:%s:%s/%s/%s/%s", region, accountId, apiId, stage, httpMethod, "*");
-        if(primaryToken.equals("123") && secondaryToken.equals("456")) {
-            logger.log("Allow " + arn);
-            return new AuthPolicy(principalId, getAllowOnePolicy(region, accountId, apiId, stage, AuthPolicy.HttpMethod.valueOf(httpMethod), "*"), ctx);
-        } else {
-            logger.log("Deny " + arn);
+        String principalId = null;
+        Map<String, String> headers = request.getHeaders();
+        String authorization = headers.get("Authorization");
+        String scopeToken = headers.get("X-Scope-Token");
+        String primaryToken = authorization.substring(7);
+        String secondaryToken = scopeToken == null ? null : scopeToken.substring(7);
+        Map<String, String> ctx = new HashMap<>();
+        try {
+            JwtClaims claims = jwtVerifier.verifyJwt(primaryToken, false);
+            // handle the primary token.
+            String clientId = claims.getStringClaimValue(Constants.CLIENT_ID_STRING);
+            // try to get the cid as some OAuth tokens name it as cid like Okta.
+            if(clientId == null) clientId = claims.getStringClaimValue(Constants.CID_STRING);
+            ctx.put(Constants.CLIENT_ID_STRING, clientId);
+            String userId = claims.getStringClaimValue(Constants.USER_ID_STRING);
+            // try to get the uid as some OAuth tokens name it as uid like Okta.
+            if(userId == null) userId = claims.getStringClaimValue(Constants.UID_STRING);
+            if(userId != null) {
+                ctx.put(Constants.USER_ID_STRING, userId);
+                principalId = userId;
+            } else {
+                principalId = clientId;
+            }
+            // primary scopes
+            List<String> primaryScopes = null;
+            Object scopeClaim = claims.getClaimValue(Constants.SCOPE_STRING);
+            if(scopeClaim instanceof String) {
+                primaryScopes = Arrays.asList(claims.getStringClaimValue(Constants.SCOPE_STRING).split(" "));
+            } else if(scopeClaim instanceof List) {
+                primaryScopes = claims.getStringListClaimValue(Constants.SCOPE_STRING);
+            }
+            if(primaryScopes == null || primaryScopes.isEmpty()) {
+                // some IDPs like Okta and Microsoft call scope claim "scp" instead of "scope"
+                Object scpClaim = claims.getClaimValue(Constants.SCP_STRING);
+                if(scpClaim instanceof String) {
+                    primaryScopes = Arrays.asList(claims.getStringClaimValue(Constants.SCP_STRING).split(" "));
+                } else if(scpClaim instanceof List) {
+                    primaryScopes = claims.getStringListClaimValue(Constants.SCP_STRING);
+                }
+            }
+            ctx.put(Constants.PRIMARY_SCOPES, primaryScopes.toString());
+        } catch (InvalidJwtException e) {
+            logger.error("InvalidJwtException:", e);
+            logger.error(new Status(STATUS_INVALID_AUTH_TOKEN).toString());
+            return new AuthPolicy(principalId, getDenyOnePolicy(region, accountId, apiId, stage, AuthPolicy.HttpMethod.valueOf(httpMethod), "*"), ctx);
+        } catch (ExpiredTokenException e) {
+            logger.error("ExpiredTokenException", e);
+            logger.error(new Status(STATUS_AUTH_TOKEN_EXPIRED).toString());
+            return new AuthPolicy(principalId, getDenyOnePolicy(region, accountId, apiId, stage, AuthPolicy.HttpMethod.valueOf(httpMethod), "*"), ctx);
+        } catch (MalformedClaimException e) {
+            logger.error("MalformedClaimException", e);
+            logger.error(new Status(STATUS_INVALID_AUTH_TOKEN).toString());
             return new AuthPolicy(principalId, getDenyOnePolicy(region, accountId, apiId, stage, AuthPolicy.HttpMethod.valueOf(httpMethod), "*"), ctx);
         }
+        logger.debug("Allow " + arn);
+        return new AuthPolicy(principalId, getAllowOnePolicy(region, accountId, apiId, stage, AuthPolicy.HttpMethod.valueOf(httpMethod), "*"), ctx);
     }
 }

4504ebb65d6fc3f7e835185394b234455dee3be9
Steve Hu <stevehu@gmail.com>
2020-12-17 17:24:11 -0500

    fixes #15 update evn-config to singleton and use ClassLoader resource

--- env-config/src/main/java/com/networknt/aws/lambda/Configuration.java
+++ env-config/src/main/java/com/networknt/aws/lambda/Configuration.java
@@ -7,18 +7,24 @@ import java.util.Map;
 
 public class Configuration {
     private final Yaml yaml = new Yaml();
-    private Map<String, Map<String, Object>> configuration;
+    private Map<String, Map<String, Object>> config;
+    private static Configuration INSTANCE = new Configuration();
 
-    public Configuration() {
+    public static Configuration getInstance() {
+        return INSTANCE;
+    }
+
+    private Configuration() {
         load();
     }
 
     private void load() {
-        InputStream inputStream = Configuration.class.getResourceAsStream("/app.yml");
-        configuration = yaml.load(inputStream);
+        InputStream inputStream = Configuration.class.getClassLoader().getResourceAsStream("app.yml");
+        config = yaml.load(inputStream);
     }
 
-    public Map<String, Object> getConfigMap(String env) {
-        return configuration.get(env);
+    public Map<String, Object> getStageConfig(String env) {
+        return config.get(env);
     }
+    public Map<String, Map<String, Object>> getConfig() { return config; }
 }

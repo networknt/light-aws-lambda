1e5245c26dc35b18d162d86f0cd1a77491433da6
Steve Hu <stevehu@gmail.com>
2020-12-20 22:29:38 -0500

    fixes #26 integrate the scope verifier and openapi validator in the request handler

--- request-handler/src/main/java/com/networknt/aws/lambda/LightRequestHandler.java
+++ request-handler/src/main/java/com/networknt/aws/lambda/LightRequestHandler.java
@@ -4,17 +4,17 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class LightRequestHandler {
     public APIGatewayProxyResponseEvent interceptRequest(final APIGatewayProxyRequestEvent input, final Context context) {
-        // validate, enrich the input and context. If error, return
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", "application/json");
-        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent().withHeaders(headers);
-
-        return response;
+        // JWT scope verification against the openapi specification.
+        ScopeVerifier scopeVerifier = new ScopeVerifier();
+        APIGatewayProxyResponseEvent responseEvent = scopeVerifier.verifyScope(input);
+        if(responseEvent != null) return responseEvent;
+        // OpenAPI schema validation
+        LambdaSchemaValidator validator = new LambdaSchemaValidator();
+        responseEvent = validator.validateRequest(input);
+        if(responseEvent != null) return responseEvent;
+        return null;
     }
 
     public APIGatewayProxyResponseEvent interceptResponse(APIGatewayProxyResponseEvent response) {

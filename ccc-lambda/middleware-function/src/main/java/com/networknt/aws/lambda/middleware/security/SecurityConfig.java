0319ba9faa3001984bc17157027cf2529a0eaff1
Steve Hu <stevehu@gmail.com>
2023-08-30 10:25:09 -0400

    fixes #68 update the SecurityConfig in sync with light-4j security config

--- ccc-lambda/middleware-function/src/main/java/com/networknt/aws/lambda/middleware/security/SecurityConfig.java
+++ ccc-lambda/middleware-function/src/main/java/com/networknt/aws/lambda/middleware/security/SecurityConfig.java
@@ -1,8 +1,20 @@
 package com.networknt.aws.lambda.middleware.security;
 
+import java.util.List;
+import java.util.Map;
+
 public class SecurityConfig {
+    public static final String CONFIG_NAME = "lambda-security";
+
+    public static final String CLOCK_SKEW_IN_SECONDS = "clockSkewInSeconds";
 
     boolean enableVerifyJwt;
+
+    boolean enableVerifySwt;
+
+    String swtClientIdHeader = "swt-client";
+
+    String swtClientSecretHeader = "swt-secret";
 
     // Extract JWT scope token from the X-Scope-Token header and validate the JWT token
     boolean enableExtractScopeToken;
@@ -10,6 +22,8 @@ public class SecurityConfig {
     // Enable JWT scope verification.
     // Only valid when (enableVerifyJwt is true) AND (enableVerifyJWTScopeToken is true)
     boolean enableVerifyScope;
+
+    boolean skipVerifyScopeWithoutSpec;
 
     // User for test only. should be always be false on official environment.
     boolean enableMockJwt;
@@ -27,12 +41,33 @@ public class SecurityConfig {
     // and skip the signature verification as it takes more CPU power and long time.
     boolean enableJwtCache;
 
+    int jwtCacheFullSize;
+
+    boolean bootstrapFromKeyService;
+
+    List<String> skipPathPrefixes;
+
+    Map<String, String> passThroughClaims;
+    Map<String, Object> jwt;
+
     public boolean getIgnoreJwtExpiry() {
         return ignoreJwtExpiry;
     }
 
     public boolean isEnableVerifyJwt() {
         return enableVerifyJwt;
+    }
+
+    public boolean isEnableVerifySwt() {
+        return enableVerifySwt;
+    }
+
+    public String getSwtClientIdHeader() {
+        return swtClientIdHeader;
+    }
+
+    public String getSwtClientSecretHeader() {
+        return swtClientSecretHeader;
     }
 
     public boolean isEnableExtractScopeToken() {
@@ -61,5 +96,29 @@ public class SecurityConfig {
 
     public boolean isEnableJwtCache() {
         return enableJwtCache;
+    }
+
+    public boolean isSkipVerifyScopeWithoutSpec() {
+        return skipVerifyScopeWithoutSpec;
+    }
+
+    public int getJwtCacheFullSize() {
+        return jwtCacheFullSize;
+    }
+
+    public boolean isBootstrapFromKeyService() {
+        return bootstrapFromKeyService;
+    }
+
+    public List<String> getSkipPathPrefixes() {
+        return skipPathPrefixes;
+    }
+
+    public Map<String, String> getPassThroughClaims() {
+        return passThroughClaims;
+    }
+
+    public Map<String, Object> getJwt() {
+        return jwt;
     }
 }

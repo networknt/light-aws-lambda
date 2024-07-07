db624147d7f5d5a091bd577047f3db1c4b4fc42d
Steve Hu <stevehu@gmail.com>
2024-07-07 12:36:48 -0400

    fixes #127 add lambda-utility and schema-validator to share with native (#128)

---
+++ schema-validator/src/main/java/com/networknt/aws/lambda/validator/ParameterType.java
@@ -0,0 +1,28 @@
+package com.networknt.aws.lambda.validator;
+
+import com.networknt.utility.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum ParameterType {
+	PATH,
+	QUERY,
+	HEADER;
+
+	private static final Map<String, ParameterType> lookup = new HashMap<>();
+
+	static {
+		for (ParameterType type: ParameterType.values()) {
+			lookup.put(type.name(), type);
+		}
+	}
+
+	public static ParameterType of(String typeStr) {
+		return lookup.get(StringUtils.trimToEmpty(typeStr).toUpperCase());
+	}
+
+	public static boolean is(String typeStr, ParameterType type) {
+		return type == of(typeStr);
+	}
+}

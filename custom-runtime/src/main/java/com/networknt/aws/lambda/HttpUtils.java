1866b8e704d4f8cb00039394b4394a268fca52fc
Kalev Gonvick <kalev.gonvick@outlook.com>
2026-03-18 12:09:00 -0700

    Changed method from writeBytes to write to avoid mangling accent characters

--- custom-runtime/src/main/java/com/networknt/aws/lambda/HttpUtils.java
+++ custom-runtime/src/main/java/com/networknt/aws/lambda/HttpUtils.java
@@ -4,8 +4,13 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class HttpUtils {
+
+    private HttpUtils() {
+        throw new IllegalStateException("HttpUtils is a utility class");
+    }
 
     public static HttpURLConnection get(String endpoint) throws IOException {
         URL url = new URL(endpoint);
@@ -28,7 +33,7 @@ public class HttpUtils {
         connection.setDoOutput(true);
 
         DataOutputStream out = new DataOutputStream(connection.getOutputStream());
-        out.writeBytes(message);
+        out.write(message.getBytes(StandardCharsets.UTF_8));
 
         out.flush();
         out.close();
